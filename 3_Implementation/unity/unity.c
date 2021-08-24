416c7704524024d969cf6c2f1031ff8258f6f19b
chandana <chandanasraj077@gmail.com>
2021-08-24 22:40:46 +0530

    commit

--- 3_Implementation/unity/unity.c
+++ 3_Implementation/unity/unity.c
@@ -1,9 +1,11 @@
-/* =========================================================================
-    Unity Project - A Test Framework for C
-    Copyright (c) 2007-19 Mike Karlesky, Mark VanderVoord, Greg Williams
-    [Released under MIT License. Please refer to license.txt for details]
-============================================================================ */
-
+/**
+ * @file unity.c
+ * @version 0.1
+ * @date 2021-08-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include "unity.h"
 #include <stddef.h>
 
